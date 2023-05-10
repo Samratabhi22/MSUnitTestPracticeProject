04d420de8a85580e42b22079a53de4567eb1f106
Samratabhi <mech.abhi22@gmail.com>
2023-05-10 10:50:52 +0530

    Add project files.

---
+++ MSUnitTestPracticeProject/BaseClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSUnitTestPracticeProject
+{
+    [TestClass]
+    public class BaseClass
+    {
+        [AssemblyInitialize]
+        public static  void BeforeProject(TestContext context)
+        {
+            Console.WriteLine("Before Project");
+        }
+        [AssemblyCleanup]
+        public static void AfterProject()
+        {
+            Console.WriteLine("After Project");
+        }
+    }
+}
